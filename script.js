5e5014db518682a7a75dad353bc042694ee2913b
Aleks <example@gmail.com>
2019-07-31 16:25:26 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// var one =1;
+// console.log(one);
+// console.log(4/0);
+// console.log('string'*9);
+// var string ="Hello";
+// console.log(string);
+// var obj ={};
+// let persone ={
+//     name: "Jone",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(persone["name"]);
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+// console.log(arr[0]);
+// alert("Hello World!");
+// confirm("Are you here?");
+
+
+// let answer =+prompt("Aare yoy 18?", "Yes");
+// console.log(typeof(answer));
+
+let incr=10,
+    decr =10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2"===2);
